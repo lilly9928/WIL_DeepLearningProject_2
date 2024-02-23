e01b57228f3249772188573a5b54e98e92f48416
Jieun Kim <lilly9928@icloud.com>
2024-02-23 21:58:50 +0900

    update

---
+++ RSG/vcr_dataloader/config.py
@@ -0,0 +1,12 @@
+import os
+USE_IMAGENET_PRETRAINED = True # otherwise use detectron, but that doesnt seem to work?!?
+
+# Change these to match where your annotations and images are
+VCR_IMAGES_DIR = os.path.join('/data2/KJE/VCR', 'vcr1images')
+VCR_ANNOTS_DIR = os.path.join( '/data2/KJE/VCR')
+VCR_DATALOADER_DIR = os.path.join( '/home/user2/code/WIL_DeepLearningProject_2/CommonSense/base/r2c/')
+
+
+
+if not os.path.exists(VCR_IMAGES_DIR):
+    raise ValueError("Update config.py with where you saved VCR images to.")
--- Study/gpt-neox-main/tests/unit/test_dependencies.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from megatron import fused_kernels
-
-
-def test_fused_kernels():
-    pytest.xfail(reason="Fused kernels require manual intervention to install")
-    fused_kernels.load_fused_kernels()
